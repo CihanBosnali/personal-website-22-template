ccaff1a6a9399bf6349094dad65593538d5c808b
Cihan Bosnali <cihanbosnali@gmail.com>
2022-07-07 02:54:37 +0300

    Use json file to add user data to the website

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,14 +3,19 @@ import SmallCard from "./SmallCard";
 
 const Section = ({sectionName, cards}) => {
 
-    const cardComponents = cards.map((card) => <MediumCard key={card}/>);
+    const cardComponents = cards.map((card) => {
+        if (card.size == "small") {
+            return <SmallCard/>;
+        } else {
+            return <MediumCard/>;
+        }
+    });
 
     return (
         <div className="section">
             <h4>{sectionName}</h4>
             <div className="card-deck">
                 {cardComponents}
-                <SmallCard/>
             </div>
         </div>
     )
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,15 +3,17 @@ import Header from "../components/Header";
 import LargeCard from "../components/LargeCard";
 import Section from "../components/Section";
 
+import data from "../data/data.json"
+
 const Homepage = () => {
 
+    const sections = data.sections.map((section) => <Section sectionName={section.name} cards={section.cards}/>) 
+    
     return(
         <div className="App">
-            <Header name="Name Surname"/>
+            <Header name={data.name}/>
             <LargeCard/>
-            <Section sectionName="Education" cards={["1","2"]}/>
-            <Section sectionName="Projects" cards={["1","2","3","4"]}/>
-            <Section sectionName="Contact and Social Media" cards={["1","2"]}/>
+            {sections}
             <Footer/>
 
         </div>
