6098f0fd47911661513b7dac5127b85d3749cc95
Cihan Bosnali <cihanbosnali@gmail.com>
2022-07-07 04:09:14 +0300

    Divide data to multiple json files

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Searchpage from './pages/Searchpage';
 
+import mainData from "./data/main.json";
 
 function App() {
+  document.title = mainData.name;
+
   return (
     <Routes>
-      <Route path='/' element={<Homepage/>}/>
+      <Route path='/' element={<Homepage name={mainData.name}/>}/>
       <Route path='/search' element={<Searchpage/>}/>
     </Routes>
   );
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,15 +3,14 @@ import Header from "../components/Header";
 import LargeCard from "../components/LargeCard";
 import Section from "../components/Section";
 
-import data from "../data/data.json"
+import homeData from "../data/homepage.json"
 
-const Homepage = () => {
-
-    const sections = data.sections.map((section) => <Section sectionName={section.name} cards={section.cards}/>) 
+const Homepage = ({name}) => {
+    const sections = homeData.sections.map((section) => <Section sectionName={section.name} cards={section.cards}/>);
     
     return(
         <div className="App">
-            <Header name={data.name}/>
+            <Header name={name}/>
             <LargeCard/>
             {sections}
             <Footer/>
